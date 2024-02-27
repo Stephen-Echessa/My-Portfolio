529d9259812e01faec8840b52a05e3ba73834bbd
eastberry <albanus@eastberry.io>
2024-02-27 06:50:58 +0300

    c

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Home from './components/Home';
 import Work from './components/Work';
 import Sidenav from './components/Sidenav'
 import Projects from './components/Projects';
-import Contact from './components/contact';
+import Contact from './components/Contact';
 import Footer from './components/footer';
 
 function App() {
